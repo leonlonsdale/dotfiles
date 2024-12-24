4b9474fede6f0a6dade7ad0b490c175adf19ecf0
Leon <coding@leonlonsdale.dev>
2024-12-24 15:11:06 +0000

    fix blink binds

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -4,9 +4,11 @@ return {
 	dependencies = "rafamadriz/friendly-snippets",
 	version = "v0.*",
 	opts = {
-		-- keymap = {
-		-- 	["<S-Enter>"] = { "accept" },
-		-- },
+		keymap = {
+			-- ["<S-Enter>"] = { "accept" },
+			["<Up>"] = { "select_prev", "fallback" },
+			["<Down>"] = { "select_next", "fallback" },
+		},
 		appearance = {
 			use_nvim_cmp_as_default = true,
 			nerd_font_variant = "mono",
