8a7f41946f582b01298f01c4f0c2444f8743d9c0
Leon <coding@leonlonsdale.dev>
2024-12-29 20:51:44 +0000

    switch themes to catppuccin

--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -17,67 +17,87 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	spec = {
-		{
-			"ellisonleao/gruvbox.nvim",
-			priority = 1000 , 
-			config = function ()
-				require('gruvbox').setup({
-					terminal_colors = true,
-					undercurl = true,
-					transparent_mode = true,
-				})
-				vim.cmd("colorscheme gruvbox")
-			end,
-						
-		},
 		-- {
-		-- 	"catppuccin/nvim",
-		-- 	name = "catppuccin",
+		-- 	"folke/tokyonight.nvim",
+		-- 	lazy = false,
 		-- 	priority = 1000,
 		-- 	config = function()
-		-- 		require("catppuccin").setup({
-		-- 			flavour = "mocha",
-		-- 			transparent_background = true,
-		-- 			integrations = {
-		-- 				neotree = true,
-		-- 				-- gitsigns = true,
-		-- 				-- harpoon = true,
-		-- 				blink_cmp = true,
-		-- 				fzf = true,
-		-- 				mason = true,
-		-- 				markdown = true,
-		-- 				mini = {
-		-- 					enabled = true,
-		-- 					indentscope_color = "lavender",
-		-- 				},
-		-- 				native_lsp = {
-		-- 					enabled = true,
-		-- 					virtual_text = {
-		-- 						errors = { "italic" },
-		-- 						hints = { "italic" },
-		-- 						warnings = { "italic" },
-		-- 						information = { "italic" },
-		-- 						ok = { "italic" },
-		-- 					},
-		-- 					underlines = {
-		-- 						errors = { "underline" },
-		-- 						hints = { "underline" },
-		-- 						warnings = { "underline" },
-		-- 						information = { "underline" },
-		-- 						ok = { "underline" },
-		-- 					},
-		-- 					inlay_hints = {
-		-- 						background = true,
-		-- 					},
-		-- 				},
-		-- 				lsp_trouble = true,
-		-- 				treesitter = true,
-		-- 				treesitter_context = true,
+		-- 		require("tokyonight").setup({
+		-- 			style = "night",
+		-- 			transparent = true,
+		-- 			terminal_colors = true,
+		-- 			styles = {
+		-- 				floats = "transparent",
+		-- 				sidebars = "transparent",
+		-- 				comments = { italic = false },
+		-- 				keywords = { italic = false },
+		-- 				variables = { bold = true },
 		-- 			},
+		-- 			dim_inactive = true,
 		-- 		})
-		-- 		vim.cmd.colorscheme("catppuccin")
+		-- 		vim.cmd.colorscheme("tokyonight")
 		-- 	end,
 		-- },
+		-- {
+		-- 	"ellisonleao/gruvbox.nvim",
+		-- 	priority = 1000,
+		-- 	config = function()
+		-- 		require("gruvbox").setup({
+		-- 			terminal_colors = true,
+		-- 			undercurl = true,
+		-- 			transparent_mode = true,
+		-- 		})
+		-- 		vim.cmd("colorscheme gruvbox")
+		-- 	end,
+		-- },
+		{
+			"catppuccin/nvim",
+			name = "catppuccin",
+			priority = 1000,
+			config = function()
+				require("catppuccin").setup({
+					flavour = "mocha",
+					transparent_background = true,
+					integrations = {
+						neotree = true,
+						-- gitsigns = true,
+						-- harpoon = true,
+						blink_cmp = true,
+						fzf = true,
+						mason = true,
+						markdown = true,
+						mini = {
+							enabled = true,
+							indentscope_color = "lavender",
+						},
+						native_lsp = {
+							enabled = true,
+							virtual_text = {
+								errors = { "italic" },
+								hints = { "italic" },
+								warnings = { "italic" },
+								information = { "italic" },
+								ok = { "italic" },
+							},
+							underlines = {
+								errors = { "underline" },
+								hints = { "underline" },
+								warnings = { "underline" },
+								information = { "underline" },
+								ok = { "underline" },
+							},
+							inlay_hints = {
+								background = true,
+							},
+						},
+						lsp_trouble = true,
+						treesitter = true,
+						treesitter_context = true,
+					},
+				})
+				vim.cmd.colorscheme("catppuccin")
+			end,
+		},
 		{ import = "plugins" },
 	},
 	change_detection = {
