bad6ba48a8c58526db0c77af035ed5a6344ab94a
Leon <coding@leonlonsdale.dev>
2025-01-08 15:01:47 +0000

    update

--- nvim/lua/plugins/lsp/languages/python.lua
+++ nvim/lua/plugins/lsp/languages/python.lua
@@ -1,12 +1,5 @@
 return function()
 	sennvim.lsp.add_config("pyright", {})
-	sennvim.formatters.add_formatter("python", { "black" })
 	sennvim.linters.add_linter("python", { "ruff" })
-	-- sennvim.formatters.add_formatter("python", { "ruff" })
-	-- sennvim.formatters.add_formatter_config("black", {
-	-- 	filetypes = { "python" },
-	-- 	prepend_args = { "--fast" },
-	-- 	args = { "--quiet" },
-	-- 	stdin = true,
-	-- })
+	sennvim.formatters.add_formatter("python", { "ruff" })
 end
