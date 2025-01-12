caa772658a853e022f9894e471269f9c58460902
Leon <coding@leonlonsdale.dev>
2025-01-12 18:45:54 +0000

    updates
    add sketchy bar
    more ghostty opacity options

--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -17,33 +17,33 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	spec = {
-		{
-			"folke/tokyonight.nvim",
-			lazy = false,
-			priority = 1000,
-			config = function()
-				require("tokyonight").setup({
-					style = "storm",
-					transparent = true,
-					terminal_colors = true,
-					styles = {
-						floats = "transparent",
-						sidebars = "transparent",
-						comments = { italic = false },
-						keywords = { italic = false },
-						variables = { bold = true },
-					},
-					on_highlights = function(highlights, colors)
-						highlights.ColorColumn = { bg = "#46215F" }
-						-- highlights.RenderMarkdownCode = { bg = "NONE" }
-					end,
-					dim_inactive = true,
-				})
-				vim.cmd.colorscheme("tokyonight")
-
-				-- vim.cmd([[highlight ColorColumn ctermbg=lightgrey guibg=#47215F]])
-			end,
-		},
+		-- {
+		-- 	"folke/tokyonight.nvim",
+		-- 	lazy = false,
+		-- 	priority = 1000,
+		-- 	config = function()
+		-- 		require("tokyonight").setup({
+		-- 			style = "storm",
+		-- 			transparent = true,
+		-- 			terminal_colors = true,
+		-- 			styles = {
+		-- 				floats = "transparent",
+		-- 				sidebars = "transparent",
+		-- 				comments = { italic = false },
+		-- 				keywords = { italic = false },
+		-- 				variables = { bold = true },
+		-- 			},
+		-- 			on_highlights = function(highlights, colors)
+		-- 				highlights.ColorColumn = { bg = "#46215F" }
+		-- 				-- highlights.RenderMarkdownCode = { bg = "NONE" }
+		-- 			end,
+		-- 			dim_inactive = true,
+		-- 		})
+		-- 		vim.cmd.colorscheme("tokyonight")
+		--
+		-- 		-- vim.cmd([[highlight ColorColumn ctermbg=lightgrey guibg=#47215F]])
+		-- 	end,
+		-- },
 		-- {
 		-- 	"ellisonleao/gruvbox.nvim",
 		-- 	priority = 1000,
@@ -56,54 +56,54 @@ require("lazy").setup({
 		-- 		vim.cmd("colorscheme gruvbox")
 		-- 	end,
 		-- },
-		-- {
-		-- 	"catppuccin/nvim",
-		-- 	name = "catppuccin",
-		-- 	priority = 1000,
-		-- 	config = function()
-		-- 		require("catppuccin").setup({
-		-- 			flavour = "macchiato",
-		-- 			transparent_background = true,
-		-- 			integrations = {
-		-- 				neotree = true,
-		-- 				-- gitsigns = true,
-		-- 				-- harpoon = true,
-		-- 				blink_cmp = true,
-		-- 				fzf = true,
-		-- 				mason = true,
-		-- 				markdown = true,
-		-- 				mini = {
-		-- 					enabled = true,
-		-- 					indentscope_color = "lavender",
-		-- 				},
-		-- 				native_lsp = {
-		-- 					enabled = true,
-		-- 					virtual_text = {
-		-- 						errors = { "italic" },
-		-- 						hints = { "italic" },
-		-- 						warnings = { "italic" },
-		-- 						information = { "italic" },
-		-- 						ok = { "italic" },
-		-- 					},
-		-- 					underlines = {
-		-- 						errors = { "underline" },
-		-- 						hints = { "underline" },
-		-- 						warnings = { "underline" },
-		-- 						information = { "underline" },
-		-- 						ok = { "underline" },
-		-- 					},
-		-- 					inlay_hints = {
-		-- 						background = true,
-		-- 					},
-		-- 				},
-		-- 				lsp_trouble = true,
-		-- 				treesitter = true,
-		-- 				treesitter_context = true,
-		-- 			},
-		-- 		})
-		-- 		vim.cmd.colorscheme("catppuccin")
-		-- 	end,
-		-- },
+		{
+			"catppuccin/nvim",
+			name = "catppuccin",
+			priority = 1000,
+			config = function()
+				require("catppuccin").setup({
+					flavour = "mocha",
+					transparent_background = true,
+					integrations = {
+						neotree = true,
+						-- gitsigns = true,
+						-- harpoon = true,
+						blink_cmp = true,
+						fzf = true,
+						mason = true,
+						markdown = true,
+						mini = {
+							enabled = true,
+							indentscope_color = "lavender",
+						},
+						native_lsp = {
+							enabled = true,
+							virtual_text = {
+								errors = { "italic" },
+								hints = { "italic" },
+								warnings = { "italic" },
+								information = { "italic" },
+								ok = { "italic" },
+							},
+							underlines = {
+								errors = { "underline" },
+								hints = { "underline" },
+								warnings = { "underline" },
+								information = { "underline" },
+								ok = { "underline" },
+							},
+							inlay_hints = {
+								background = true,
+							},
+						},
+						lsp_trouble = true,
+						treesitter = true,
+						treesitter_context = true,
+					},
+				})
+				vim.cmd.colorscheme("catppuccin")
+			end,
+		},
 		-- {
 		-- 	"rebelot/kanagawa.nvim",
 		-- 	priority = 1000,
