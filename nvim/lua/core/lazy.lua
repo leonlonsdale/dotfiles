bbbdf199d0321bf483653f25991bcdbe069cc9f1
Leon <coding@leonlonsdale.dev>
2025-01-21 19:40:12 +0000

    use ayu mirage theme in neovim

--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -18,20 +18,40 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	spec = {
 		{
-			"projekt0n/github-nvim-theme",
-			name = "github-theme",
-			lazy = false,
-			priority = 1000,
+			"Shatur/neovim-ayu",
 			config = function()
-				require("github-theme").setup({
-					options = {
-
-						transparent = true,
-					},
+				require("ayu").setup({
+					mirage = true, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
+					terminal = true, -- Set to `false` to let terminal manage its own colors.
+					overrides = {
+						Normal = { bg = "None" },
+						NormalFloat = { bg = "none" },
+						SignColumn = { bg = "None" },
+						Folded = { bg = "None" },
+						FoldColumn = { bg = "None" },
+						CursorLine = { bg = "None" },
+						CursorColumn = { bg = "None" },
+						VertSplit = { bg = "None" },
+					}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
 				})
-				vim.cmd("colorscheme github_dark_dimmed")
+				vim.cmd("colorscheme ayu")
 			end,
 		},
+		-- {
+		-- 	"projekt0n/github-nvim-theme",
+		-- 	name = "github-theme",
+		-- 	lazy = false,
+		-- 	priority = 1000,
+		-- 	config = function()
+		-- 		require("github-theme").setup({
+		-- 			options = {
+		--
+		-- 				transparent = true,
+		-- 			},
+		-- 		})
+		-- 		vim.cmd("colorscheme github_dark_dimmed")
+		-- 	end,
+		-- },
 		-- {
 		-- 	"folke/tokyonight.nvim",
 		-- 	lazy = false,
