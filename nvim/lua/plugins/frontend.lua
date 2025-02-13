506b318fe8f589194578b55841ac02a0abfab920
Leon <coding@leonlonsdale.dev>
2025-02-13 19:02:05 +0000

    add live preview to frontend

--- nvim/lua/plugins/frontend.lua
+++ nvim/lua/plugins/frontend.lua
@@ -23,4 +23,14 @@ return {
 			})
 		end,
 	},
+	{
+		"brianhuster/live-preview.nvim",
+		dependencies = {
+			-- You can choose one of the following pickers
+			"ibhagwan/fzf-lua",
+		},
+		config = function()
+			require("livepreview.config").set({})
+		end,
+	},
 }
