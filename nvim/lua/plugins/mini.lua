9a45aa6980852b2f30c495bf144e76631b282528
Leon <coding@leonlonsdale.dev>
2025-02-21 18:04:35 +0000

    reinstate mini statusline

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"echasnovski/mini.nvim",
 		config = function()
-			-- local statusline = require("mini.statusline")
+			local statusline = require("mini.statusline")
 			local icons = require("mini.icons")
 			local pairs = require("mini.pairs")
 			local indentscope = require("mini.indentscope")
@@ -13,32 +13,32 @@ return {
 
 			-- configuration funcs
 
-			-- local content_active = function()
-			-- 	local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
-			-- 	local git = statusline.section_git({ trunc_width = 40 })
-			-- 	local diff = statusline.section_diff({ trunc_width = 75 })
-			-- 	local diagnostics = statusline.section_diagnostics({ trunc_width = 75 })
-			-- 	local lsp = statusline.section_lsp({ trunc_width = 75 })
-			-- 	-- Always show truncated file name (i.e. relative to current working directory)
-			-- 	local filename = statusline.section_filename({ trunc_width = 9999 })
-			-- 	local fileinfo = statusline.section_fileinfo({ trunc_width = 120 })
-			-- 	local location = statusline.section_location({ trunc_width = 75 })
-			-- 	local search = statusline.section_searchcount({ trunc_width = 75 })
-			--
-			-- 	return statusline.combine_groups({
-			-- 		{ hl = mode_hl, strings = { mode } },
-			-- 		{ hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics, lsp } },
-			-- 		"%<", -- Mark general truncate point
-			-- 		{ hl = "MiniStatuslineFilename", strings = { filename } },
-			-- 		"%=", -- End left alignment
-			-- 		{ hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
-			-- 		{ hl = mode_hl, strings = { search, location } },
-			-- 	})
-			-- end
+			local content_active = function()
+				local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
+				local git = statusline.section_git({ trunc_width = 40 })
+				local diff = statusline.section_diff({ trunc_width = 75 })
+				local diagnostics = statusline.section_diagnostics({ trunc_width = 75 })
+				local lsp = statusline.section_lsp({ trunc_width = 75 })
+				-- Always show truncated file name (i.e. relative to current working directory)
+				local filename = statusline.section_filename({ trunc_width = 9999 })
+				local fileinfo = statusline.section_fileinfo({ trunc_width = 120 })
+				local location = statusline.section_location({ trunc_width = 75 })
+				local search = statusline.section_searchcount({ trunc_width = 75 })
+
+				return statusline.combine_groups({
+					{ hl = mode_hl, strings = { mode } },
+					{ hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics, lsp } },
+					"%<", -- Mark general truncate point
+					{ hl = "MiniStatuslineFilename", strings = { filename } },
+					"%=", -- End left alignment
+					{ hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+					{ hl = mode_hl, strings = { search, location } },
+				})
+			end
 
 			-- setups
 			icons.setup({})
-			-- statusline.setup({ use_icons = true, content = { active = content_active } })
+			statusline.setup({ use_icons = true, content = { active = content_active } })
 			ai.setup({ n_lines = 500 })
 			pairs.setup({
 				modes = { insert = true, command = true, terminal = false },
