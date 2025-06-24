5c1111f4f1d2b2e4adb0ebe670774e904ab7bdf7
Leon <coding@leonlonsdale.dev>
2025-06-24 13:24:45 +0100

    adjust font size for jb mono and add xdg config var to fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set -x XDG_CONFIG_HOME $HOME/.config
     set -g fish_greeting
     zoxide init fish | source
 end
